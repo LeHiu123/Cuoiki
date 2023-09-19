9bc29d61aa6d781fe7e4a5603284976556efcb9b
Enazuki <hieuyenthinh@gmail.com>
2023-09-19 10:06:12 +0700

    tao file

---
+++ init.lua
@@ -0,0 +1,5 @@
+do
+    ---@type rech.scenecontrol.SingleLine
+    local SingleLine = require("singleline")
+    SingleLine.CreateSingleLine("emu_school_uniform_chibi-transformed.png", "default")
+end
