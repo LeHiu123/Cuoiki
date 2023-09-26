10c28e15d426418967bf6cd47b5753a9f3868335
Enazuki <hieuyenthinh@gmail.com>
2023-09-26 21:23:47 +0700

    fix init

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
-do
-    ---@type rech.scenecontrol.SingleLine
-    local SingleLine = require("singleline")
-    SingleLine.CreateSingleLine("emu_school_uniform_chibi-transformed.png", "default")
-end
+
+requie  "lehieu.lua"
